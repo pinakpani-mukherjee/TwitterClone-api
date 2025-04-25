58f3827299001bb55a254497af94947c8fa1539c
mukherjee-pinakpani <mukherjee.pinakpani14@gmail.com>
2025-04-25 11:08:26 +0900

    set up mongoose and tweet model

---
+++ src/db/mongoose.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/Twitterclone",{
+    //userNewUrlParser: true,
+    //userCreateIndex: true,
+    //userFindAndModify: false,
+    //useUnifiedTopology: true 
+})
