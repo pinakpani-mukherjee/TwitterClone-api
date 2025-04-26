2f9970da54d11353610f5600bd4f3df0dba882d6
pinakpani-mukherjee <mukherjee.pinakpani14@gmail.com>
2025-04-27 00:09:50 +0900

    forgot to add the routers

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
 
-
-mongoose.connect("mongodb://127.0.0.1:27017/Twitterclone",{
-    //userNewUrlParser: true,
-    //userCreateIndex: true,
-    //userFindAndModify: false,
-    //useUnifiedTopology: true 
-})
+mongoose.connect("mongodb://127.0.0.1:27017/Twitterclone", {
+  //userNewUrlParser: true,
+  //userCreateIndex: true,
+  //userFindAndModify: false,
+  //useUnifiedTopology: true
+});
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 require("./db/mongoose");
+
+const userRouter = require("./routers/user");
+const tweetRouter = require("./routers/tweet");
+const notificationRouter = require("./routers/notification");
+
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(userRouter);
+app.use(tweetRouter);
+app.use(notificationRouter);
+
 app.listen(port, () => {
-  console.log("Server is up on the port " + port);
+  console.log("Server is up on port " + port);
 });
